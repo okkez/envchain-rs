9658722889504748f5ac4f1a6076bebad3b27891
okkez <okkez000@gmail.com>
2024-04-20 15:17:53 +0900

    Implement export, import, list sub commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use clap::{Parser, Subcommand};
 use rpassword::prompt_password;
 use secret_service::blocking::SecretService;
 use secret_service::EncryptionType;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Write};
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 use std::{collections::HashMap, error::Error};
@@ -13,23 +16,56 @@ struct Cli {
     // target: Option<String>,
     #[command(subcommand)]
     command: Option<Commands>,
+    /// Execute commands with variables defined in the specified namespace
     namespace: Option<String>,
+    /// Commands to execute
     #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
     args: Vec<String>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Save values into the specified namespace
     Set {
+        /// The namespace of variables
         namespace: String,
+        /// Variable name list separated by comma
         #[arg(trailing_var_arg = true)]
         env_keys: Vec<String>,
     },
+    /// Execute commands with defined variables
     Run {
+        /// The namespace of variables
         namespace: String,
+        /// Commands to execute
         #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
         command: Vec<String>,
     },
+    List {},
+    /// Export secrets added by envchain to the file
+    Export {
+        /// The output file name
+        #[arg(short, long)]
+        output: String,
+    },
+    /// Import secrets from a file
+    Import {
+        /// The input file name
+        #[arg(short, long)]
+        input: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Entries {
+    entries: Vec<Entry>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Entry {
+    name: String,
+    key: String,
+    value: String,
 }
 
 fn main() {
@@ -48,6 +84,15 @@ impl<'a> Cli {
             }
             Some(Commands::Run { .. }) => {
                 self.run_command(&ss)?;
+            }
+            Some(Commands::List {}) => {
+                self.list_namespace(&ss)?;
+            }
+            Some(Commands::Export { .. }) => {
+                self.export_secrets(&ss)?;
+            }
+            Some(Commands::Import { .. }) => {
+                self.import_secrets(&ss)?;
             }
             None => {
                 self.run_command(&ss)?;
@@ -121,5 +166,80 @@ impl<'a> Cli {
                 "none"
             }
         }
+    }
+
+    fn list_namespace(&self, ss: &SecretService<'a>) -> Result<(), Box<dyn Error>> {
+        let collection = ss.get_default_collection()?;
+        let properties = HashMap::from([("xdg:schema", "envchain.EnvironmentVariable")]);
+        let items = collection.search_items(properties)?;
+        let mut namespaces: Vec<String> = items
+            .iter()
+            .map(|item| {
+                let attributes = item.get_attributes().unwrap();
+                attributes.get("name").unwrap().to_string()
+            })
+            .collect();
+        namespaces.sort();
+        namespaces.dedup();
+        namespaces
+            .iter()
+            .for_each(|namespace| println!("{}", namespace));
+
+        Ok(())
+    }
+
+    fn export_secrets(&self, ss: &SecretService<'a>) -> Result<(), Box<dyn Error>> {
+        let collection = ss.get_default_collection()?;
+        let properties = HashMap::from([("xdg:schema", "envchain.EnvironmentVariable")]);
+        let items = collection.search_items(properties)?;
+        let entries = Entries {
+            entries: items
+                .iter()
+                .map(|item| {
+                    let attributes = item.get_attributes().unwrap();
+                    Entry {
+                        name: attributes.get("name").unwrap().to_string(),
+                        key: attributes.get("key").unwrap().to_string(),
+                        value: String::from_utf8(item.get_secret().unwrap()).unwrap(),
+                    }
+                })
+                .collect(),
+        };
+        let toml = toml::to_string(&entries).unwrap();
+        if let Some(Commands::Export { output, .. }) = &self.command {
+            let mut io = File::create(output)?;
+            write!(io, "{}", toml)?;
+        } else {
+            println!("{}", toml);
+        }
+        Ok(())
+    }
+
+    fn import_secrets(&self, ss: &SecretService<'a>) -> Result<(), Box<dyn Error>> {
+        let collection = ss.get_default_collection()?;
+        if let Some(Commands::Import { input, .. }) = &self.command {
+            let mut io = File::open(input)?;
+            let mut toml = String::new();
+            io.read_to_string(&mut toml)?;
+            let entries: Result<Entries, toml::de::Error> = toml::from_str(toml.as_str());
+            match entries {
+                Ok(es) => es.entries.iter().for_each(|entry| {
+                    let properties = HashMap::from([
+                        ("key", entry.key.as_str()),
+                        ("name", entry.name.as_str()),
+                        ("xdg:schema", "envchain.EnvironmentVariable"),
+                    ]);
+                    let _ = collection.create_item(
+                        format!("{}.{}", entry.name, entry.key).as_str(),
+                        properties,
+                        entry.value.as_bytes(),
+                        true,
+                        "text/plain",
+                    );
+                }),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Ok(())
     }
 }
